5c1f1a67244189f8fe654e4977142484acdd40be
mubashir <mubashir22ali@gmail.com>
2017-08-08 12:19:20 +0500

    adding surge librarry

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,8 +6,7 @@ export default class App extends React.Component {
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>Todo App</h1>
-        <AddTodo/>
-        
+        <AddTodo/>   
       </div>
     )
   }
